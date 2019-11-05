83b5f04916269acf73e2884eae823b851f00e25e
xandersu <840595532@qq.com>
2019-11-05 08:32:31 +0800

    hello world

---
+++ src/main/java/com/xandersu/Main.java
@@ -0,0 +1,11 @@
+package com.xandersu;
+
+/**
+ * Created by lenovo on 2019/11/5.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
